6af554edf4d63b72e0886780764aea9a1c09eb1d
Sorasaki-mrdg <bilibilis@126.com>
2025-08-11 11:03:08 +0800

    screen拼写错误 判断壁纸模式应用screenPos偏移

--- Assets/Scripts/BlueArchive/SpineDragIK.cs
+++ Assets/Scripts/BlueArchive/SpineDragIK.cs
@@ -260,10 +260,13 @@ public class SpineDragIK : MonoBehaviour
         //Console_Log($"{gameObject.name} 坐标更新 合法");
 
         // 坐标转换流程保持不变
-        //Console_Log($"screemPos {screenPos.x} {screenPos.y}");
-        screenPos.x = screenPos.x - Wallpaper_Services.Real_WallpaperWindow_Position.x;
-        screenPos.y = screenPos.y + Wallpaper_Services.Real_WallpaperWindow_Position.y;
-        //Console_Log($"1screemPos {screenPos.x} {screenPos.y}");
+        //Console_Log($"screenPos {screenPos.x} {screenPos.y}");
+        if (Wallpaper_Services.Instance.is_Wallpaper_Mode)
+        {
+            screenPos.x = screenPos.x - Wallpaper_Services.Real_WallpaperWindow_Position.x;
+            screenPos.y = screenPos.y + Wallpaper_Services.Real_WallpaperWindow_Position.y;
+            //Console_Log($"1screenPos {screenPos.x} {screenPos.y}");
+        }
         Vector3 worldPos = CachedCamera.ScreenToWorldPoint(screenPos);
         Transform parent = Bone.parent;
         Vector3 localPos = parent.InverseTransformPoint(worldPos);
